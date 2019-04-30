b772c02cf456f3e0a0d275131ed1241f964076e4
Michael Moreno <michael.moreno.st@gmail.com>
2019-04-30 10:53:36 -0500

    Revert "Nada"
    This reverts commit 94448b9decdc2c186d69843389526b0e9cec4793.

--- Ginit/src/ginit/Ginit.java
+++ Ginit/src/ginit/Ginit.java
@@ -15,7 +15,7 @@ public class Ginit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Quiero shorar");
+        // TODO code application logic here
     }
     
 }
